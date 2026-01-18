fdf223f24e4323e44fe7978e2882781801359597
brainkat <kat.jeongeun.park@gmail.com>
2026-01-18 16:36:53 +0900

    reafactor: 필터 컴포넌트 홈페이지에만 나오기, 필요없는 파일 지우기 (home draft)

--- home.py
+++ home.py
@@ -3,6 +3,42 @@ import streamlit as st
 def homepage():
     st.title("CARPORT")
 
+    # 필터링 
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        age_range = st.selectbox(
+            "연령대 선택해주세요.",
+            ("10대", "20대", "30대"),
+        )
+        st.write("선택한 연령대:", age_range)
+
+    with col2:
+        genre = st.radio(
+            "사용연료",
+            ["경유", "수소"],
+            index=None,
+        )
+        st.write("You selected:", genre)
+
+    with col3:
+        gender_options = {
+        0: "여자",
+        1: "남자"
+        }
+        selection = st.pills(
+            "성별",
+            options=gender_options.keys(),
+            format_func=lambda option: gender_options[option],
+            selection_mode="single",
+        )
+        st.write(
+            "성별 선택: "
+            f"{None if selection is None else gender_options[selection]}"
+        )
+
+
+
 # 페이지 타이틀, 페이지 경로 설정
 home = st.Page(homepage, title="Home", icon=":material/home:")
 total_registered_vehicle_page = st.Page("total_registered_vehicle/total_registered_page.py", title="국내 자옫차등록현황은 어떨까?", icon=":material/description:")
@@ -17,39 +53,4 @@ pg.run()
 
 
 
-
-
-# 필터링 
-col1, col2, col3 = st.columns(3)
-
-with col1:
-    age_range = st.selectbox(
-        "연령대 선택해주세요.",
-        ("10대", "20대", "30대"),
-    )
-    st.write("선택한 연령대:", age_range)
-
-with col2:
-    genre = st.radio(
-        "사용연료",
-        ["경유", "수소"],
-        index=None,
-    )
-    st.write("You selected:", genre)
-
-with col3:
-    gender_options = {
-    0: "여자",
-    1: "남자"
-    }
-    selection = st.pills(
-        "성별",
-        options=gender_options.keys(),
-        format_func=lambda option: gender_options[option],
-        selection_mode="single",
-    )
-    st.write(
-        "성별 선택: "
-        f"{None if selection is None else gender_options[selection]}"
-    )
 
